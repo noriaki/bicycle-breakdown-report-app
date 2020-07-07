7e7b8b3fc9d9e540f136adc138472589c94262ba
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-07-07 09:24:02 +0900

    add prettier, prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,23 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 11,
+    project: './tsconfig.json',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    'react',
-  ],
+  plugins: ['@typescript-eslint', 'react'],
   root: true,
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-  }
+  },
 };
