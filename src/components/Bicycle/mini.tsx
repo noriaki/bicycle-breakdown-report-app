0085f8b8367dc06cd233bb7b57306592395a68cb
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-11-27 16:09:22 +0900

    refactor split styles

--- src/components/Bicycle/mini.tsx
+++ src/components/Bicycle/mini.tsx
@@ -4,9 +4,11 @@ import React from 'react';
 import Bicycle from '~/entities/Bicycle';
 
 // material-ui
-import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+
+// styles
+import useStyles from '~/styles/Bicycle/mini-style';
 
 type Props = {
   area: Bicycle['area'];
@@ -15,24 +17,15 @@ type Props = {
 
 const BicycleMiniComponent = ({ area, num }: Props) => {
   const value = Bicycle.toValue(area, num);
+  const styles = useStyles();
   return (
-    <Paper square>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        my={1}
-        py={0.5}
-      >
-        <Typography variant="body2" color="textSecondary">
-          自転車No：
-        </Typography>
-        <Typography variant="h6">
-          <Box letterSpacing={4} mx={1}>
-            {value}
-          </Box>
-        </Typography>
-      </Box>
+    <Paper square className={styles.container}>
+      <Typography component="span" variant="body2" color="textSecondary">
+        自転車No：
+      </Typography>
+      <Typography component="span" variant="h6" className={styles.value}>
+        {value}
+      </Typography>
     </Paper>
   );
 };
