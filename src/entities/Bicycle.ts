b9eb63c7a14ac406279a45eb0d86e3c50671fb6c
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-11-27 12:00:48 +0900

    refactor Bicycle entity

--- src/entities/Bicycle.ts
+++ src/entities/Bicycle.ts
@@ -1,22 +1,36 @@
-type Area = 'CYD' | 'TYO' | 'MNT';
+const areaKeys = ['CYD', 'TYO', 'MNT'] as const;
+type Area = typeof areaKeys[number];
+
+export const extractArea = (value: string): Area | undefined => {
+  const suffix = value.slice(0, 3);
+  if (areaKeys.includes(suffix)) {
+    return suffix;
+  }
+};
 
 export default class Bicycle {
-  readonly id: string;
   readonly area: Area;
   readonly num: number;
 
-  constructor(idOrObj?: string | { area: Area; num: number }) {
-    if (idOrObj !== undefined) {
-      if (typeof idOrObj === 'string') {
-        this.id = idOrObj;
-        this.area = idOrObj.slice(0, 3);
-        this.num = parseInt(idOrObj.slice(3), 10);
-      } else {
-        const { area, num } = idOrObj;
-        this.id = `${area}${num.toString().padStart(4, 0)}`;
-        this.area = area;
-        this.num = num;
-      }
+  constructor({ area, num }: { area: Area; num: number }) {
+    this.area = area;
+    this.num = num;
+  }
+
+  get value(): string {
+    return Bicycle.toValue(this.area, this.num);
+  }
+
+  static toValue(area: string, num: number): string {
+    return `${area}${num.toString().padStart(4, 0)}`;
+  }
+
+  static fromValue(value: string): Bicycle {
+    const area = extractArea(value);
+    const num = parseInt(value.slice(3), 10);
+    if (area && num) {
+      return new Bicycle({ area, num });
     }
+    throw new Error(`${value} is invalid format, '(CYD|TYO|MNT)00000'`);
   }
 }
